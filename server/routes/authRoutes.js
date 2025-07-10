c509aba0c47d60671b60552ab9ea21f81587e14f
Venisha Dsouza <venisha1031@gmail.com>
2025-07-10 20:10:52 +0530

    completed login registration

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const { test, registerUser, loginUser,getProfile, logoutUser } = require('../controllers/authController')
+
+//middleware
+router.use(
+    cors({
+        credentials: true,
+        origin: 'http://127.0.0.1:5500' //change localhost number
+    })
+)
+
+router.get('/', test)
+router.post('/register', registerUser)
+router.post('/login', loginUser)
+router.get('/profile', getProfile)
+router.post('/logout', logoutUser)
+
+
+module.exports = router;
